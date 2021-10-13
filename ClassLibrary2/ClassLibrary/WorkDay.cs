625328ee591c4a214dfe50a7d292959ad36b8350
Scott Schubert <scott_schubert@hotmail.com.au>
2021-10-14 07:28:05 +1030

    change directories

---
+++ ClassLibrary2/ClassLibrary/WorkDay.cs
@@ -0,0 +1,54 @@
+﻿using Common;
+using System;
+using System.ComponentModel;
+
+namespace ClassLibrary
+{
+    public class WorkDay: ILoggable, INotifyPropertyChanged
+    {
+        public WorkDay()
+        {
+
+        }
+        public WorkDay(string workDayId)
+        {
+            WorkDayId = workDayId;
+            Date = Utility.GetDate();
+            IsWorkFromHome = true;
+        }
+
+        public WorkDay(string workDayId, string dateString)
+        {
+            WorkDayId = workDayId;
+            Date = Utility.GetDate(dateString);
+            IsWorkFromHome = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string WorkDayId { get; private set; }
+
+        private DateTimeOffset date;
+        public DateTimeOffset Date
+        {
+            get { return date; }
+            set
+            {
+                date = value;
+                NotifyPropertyChanged("Date");
+            }
+        }
+
+        public bool IsWorkFromHome { get; set; }
+
+        public string Log() => $"Workday Id: {WorkDayId} Date: {Date.UtcDateTime}";
+
+        private void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+
+}
+
